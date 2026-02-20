9a390215b60955805831d28429192441cfaf997e
mmanzuoli-source <m.manzuoli@cec.com>
2026-02-20 22:23:11 +0100

    Update avatar.js

--- backend/frontend/avatar.js
+++ backend/frontend/avatar.js
@@ -70,7 +70,7 @@ export function initAvatar3D() {
       model.scale.set(scale, scale, scale);
 
       model.position.sub(center.multiplyScalar(scale));
-      model.position.y += 0.1;
+      model.position.y += 0.4;
 
       scene.add(model);
 
